fa379f734b19a016f7cae06b09c20cebaa7c697f
xiajufeng <xiajufeng@juzix.net>
2020-01-17 11:57:48 +0800

    Add the claim quantity field to the redemption details

--- wallet/src/main/java/com/juzix/wallet/component/adapter/ValidatorsAdapter.java
+++ wallet/src/main/java/com/juzix/wallet/component/adapter/ValidatorsAdapter.java
@@ -1,36 +1,15 @@
 package com.juzix.wallet.component.adapter;
 
-import android.annotation.SuppressLint;
-import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.RecyclerView;
-import android.text.TextUtils;
-import android.util.TypedValue;
 import android.view.ViewGroup;
-import android.widget.RelativeLayout;
-import android.widget.TextView;
-
-import com.juzhen.framework.util.NumberParserUtils;
-import com.juzix.wallet.App;
 import com.juzix.wallet.R;
-import com.juzix.wallet.app.Constants;
 import com.juzix.wallet.component.adapter.base.BaseViewHolder;
-import com.juzix.wallet.component.adapter.base.ViewHolder;
 import com.juzix.wallet.component.ui.OnItemClickListener;
-import com.juzix.wallet.component.widget.RoundedTextView;
-import com.juzix.wallet.component.widget.ShadowDrawable;
 import com.juzix.wallet.entity.VerifyNode;
-import com.juzix.wallet.utils.BigDecimalUtil;
-import com.juzix.wallet.utils.DensityUtil;
-import com.juzix.wallet.utils.GlideUtils;
-import com.juzix.wallet.utils.LanguageUtil;
-import com.juzix.wallet.utils.StringUtil;
 
-import java.text.NumberFormat;
 import java.util.List;
-import java.util.Locale;
 
 public class ValidatorsAdapter extends RecyclerView.Adapter<BaseViewHolder<VerifyNode>> {
 
@@ -40,6 +19,7 @@ public class ValidatorsAdapter extends RecyclerView.Adapter<BaseViewHolder<Verif
     public void setDatas(List<VerifyNode> verifyNodeList) {
         this.mVerifyNodeList = verifyNodeList;
     }
+
 
     public void setOnItemClickListener(OnItemClickListener itemClickListener) {
         this.mItemClickListener = itemClickListener;
