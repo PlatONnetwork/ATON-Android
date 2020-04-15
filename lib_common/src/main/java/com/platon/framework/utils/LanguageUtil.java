cb4fd2d5b4f5f6fde1a799f602253f208610e7de
夏巨奉 <635099471@qq.com>
2020-04-15 14:23:28 +0800

    优化语言选择逻辑

--- lib_common/src/main/java/com/platon/framework/utils/LanguageUtil.java
+++ lib_common/src/main/java/com/platon/framework/utils/LanguageUtil.java
@@ -1,7 +1,5 @@
 package com.platon.framework.utils;
 
-import android.content.Context;
-import android.content.res.Resources;
 import android.os.LocaleList;
 import android.text.TextUtils;
 
@@ -31,8 +29,8 @@ public class LanguageUtil {
                 locale = Locale.getDefault();
             }
 
-            if (TextUtils.equals(locale.getLanguage(), Locale.CHINESE.getLanguage()) || TextUtils.equals(locale.getLanguage(), Locale.ENGLISH.getLanguage())) {
-                return locale;
+            if (TextUtils.equals(locale.getLanguage(), Locale.CHINESE.getLanguage())) {
+                return new Locale(Locale.CHINESE.getLanguage(), "");
             } else {
                 return new Locale(Locale.ENGLISH.getLanguage(), "");
             }
